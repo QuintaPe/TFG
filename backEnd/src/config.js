eb8b11704125c55ff4f22e795975b9c4ad4b8bc1
Alejandro <alexquinta99@gmail.com>
2022-06-03 20:39:50 +0200

    Cloud Database

--- backEnd/src/config.js
+++ backEnd/src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
-  MONGODB_HOST: process.env.MONGODB_HOST || 'localhost',
+  MONGODB_HOST: process.env.MONGODB_HOST || 'lAlejandro22:Podenco1@cluster0.a5lan.mongodb.net',
   MONGODB_DATABASE: process.env.MONGODB_DB || 'ScoutCamp'
 }
--- backEnd/src/database.js
+++ backEnd/src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const config = require("./config");
 
-const MONGODB_URI = `mongodb://${config.MONGODB_HOST}/${config.MONGODB_DATABASE}`;
+const MONGODB_URI = `mongodb+srv://${config.MONGODB_HOST}/${config.MONGODB_DATABASE}`;
 
 mongoose
   .connect(MONGODB_URI)
